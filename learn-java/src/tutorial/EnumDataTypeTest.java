5c72b1ff2466e826305f0ee6e1d82d4db07ec604
Carolina Sitorus <if413061@gmail.com>
2018-08-14 13:27:57 +0700

    learn enum data type

---
+++ learn-java/src/tutorial/EnumDataTypeTest.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+class EnumDataType {
+    enum ExampleSize { SMALL, MEDIUM, LARGE};
+    ExampleSize size;
+}
+
+public class EnumDataTypeTest {
+    public static void main(String[] args){
+        EnumDataType enumType = new EnumDataType();
+        enumType.size = EnumDataType.ExampleSize.LARGE;
+        System.out.println("Size: " + enumType.size);
+    }
+}
